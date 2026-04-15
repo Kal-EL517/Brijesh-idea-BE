7a7ef59c451d675037ae14506c7054bb13b92343
Harshil_Darji <harshil.darji@imperoit.com>
2026-04-15 16:32:37 +0530

    Revert "add-is-delete"
    This reverts commit 4fd182592d674108e35d5b6393f9c5988e349240.

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -19,7 +19,6 @@ export interface IUser extends Document {
   createdAt: Date;
   updatedAt: Date;
   requestDate: Date;
-  isDeleted: boolean;
 }
 
 // Create schema with all fields from the interface
